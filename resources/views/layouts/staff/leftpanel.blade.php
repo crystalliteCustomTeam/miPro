2eb4e3478958dffa1508d60790e811742c60dc30
talha123ahmed <qa-ta@production-dev.com>
2024-02-21 22:55:49 +0500

    talha

--- resources/views/layouts/staff/leftpanel.blade.php
+++ resources/views/layouts/staff/leftpanel.blade.php
@@ -229,6 +229,67 @@
                 <li class="sub-item"><a href="/settings/user/client" class="sub-link">Assign Clients</a></li>
             </ul>
           </li><!-- br-menu-item -->
+
+
+          @elseif($departmentAccess[0]->access == 3)
+
+          <li class="br-menu-item">
+            <a href="#" class="br-menu-link with-sub">
+              <i class="menu-item-icon icon ion-ios-photos-outline tx-20"></i>
+              <span class="menu-item-label">Reporting</span>
+            </a><!-- br-menu-link -->
+            <ul class="br-menu-sub">
+              <li class="sub-item"><a href="/userlist" class="sub-link">KYC REPORT</a></li>
+              <li class="sub-item"><a href="/createuser" class="sub-link">Create Users</a></li>
+              <li class="sub-item"><a href="/searchuser" class="sub-link">Search Users</a></li>
+            </ul>
+          </li>
+
+
+
+          <li class="br-menu-item">
+              <a href="#" class="br-menu-link with-sub">
+                <i class="menu-item-icon icon ion-ios-filing-outline tx-24"></i>
+                <span class="menu-item-label">Client Registration</span>
+              </a><!-- br-menu-link -->
+              <ul class="br-menu-sub">
+                <li class="sub-item"><a href="/forms/kyc" class="sub-link">SEO Kyc</a></li>
+                <li class="sub-item"><a href="/forms/book" class="sub-link">Book Kyc</a></li>
+                <li class="sub-item"><a href="/forms/website" class="sub-link">Website Kyc</a></li>
+                <li class="sub-item"><a href="/forms/cld" class="sub-link">CLD Kyc</a></li>
+                <li class="sub-item"><a href="/all/clients" class="sub-link">Clients</a></li>
+              </ul>
+            </li><!-- br-menu-item -->
+            <li class="br-menu-item">
+              <a href="#" class="br-menu-link with-sub">
+                <i class="menu-item-icon ion-ios-redo-outline tx-24"></i>
+                <span class="menu-item-label">Projects</span>
+              </a><!-- br-menu-link -->
+              <ul class="br-menu-sub">
+                <li class="sub-item"><a href="/client/project" class="sub-link">Create Project</a></li>
+              </ul>
+            </li><!-- br-menu-item -->
+            <li class="br-menu-item">
+                <a href="#" class="br-menu-link with-sub">
+                  <i class="menu-item-icon icon ion-ios-gear-outline tx-24"></i>
+                  <span class="menu-item-label">QA Forms</span>
+                </a><!-- br-menu-link -->
+                <ul class="br-menu-sub">
+                  <li class="sub-item"><a href="/forms/qaform_d" class="sub-link">QA FORM</a></li>
+                </ul>
+              </li><!-- br-menu-item -->
+              <li class="br-menu-item">
+                  <a href="#" class="br-menu-link with-sub">
+                    <i class="menu-item-icon icon ion-ios-gear-outline tx-24"></i>
+                    <span class="menu-item-label">Settings</span>
+                  </a><!-- br-menu-link -->
+                  <ul class="br-menu-sub">
+                      <li class="sub-item"><a href="/settings/Production/services" class="sub-link">Production Services</a></li>
+                      <li class="sub-item"><a href="/settings/qa_issues" class="sub-link">QA Form Issues</a></li>
+                      <li class="sub-item"><a href="/settings/user/client" class="sub-link">Assign Clients</a></li>
+                  </ul>
+                </li><!-- br-menu-item -->
+
     @endif
 
 
