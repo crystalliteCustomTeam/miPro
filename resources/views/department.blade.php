ed3be09de9a71020df566942e4d425ff710d24d8
talha123ahmed <qa-ta@production-dev.com>
2024-08-02 00:40:29 +0500

    authorizationsystem

--- resources/views/department.blade.php
+++ resources/views/department.blade.php
@@ -256,6 +256,6 @@
             <a target="_blank" class="pd-x-5" href="https://twitter.com/home?status=Bracket%20Plus,%20your%20best%20choice%20for%20premium%20quality%20admin%20template%20from%20Bootstrap.%20Get%20it%20now%20at%20http%3A//themepixels.me/bracketplus/intro"><i class="fab fa-twitter tx-20"></i></a>
           </div>
         </footer>
-      </div><!-- br-mainpanel -->
+    </div><!-- br-mainpanel -->
       <!-- ########## END: MAIN PANEL ########## -->
 @endsection
